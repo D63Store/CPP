b5ccb6e8977778eeab209bed9da0ace25b22d5c0
D63Store Owner <fleefymusic@gmail.com>
2024-06-04 22:04:15 +0300

    Update Task-8.5.cpp

--- Task-8.5.cpp
+++ Task-8.5.cpp
@@ -2,14 +2,34 @@
 
 using namespace std;
 
- template <typename AnyType>
- AnyType add(AnyType First, AnyType Second) {
-	 return First + Second;
- }
+template <typename AnyType>
+AnyType add(AnyType First, AnyType Second) {
+	return First + Second;
+}
+
+
+template <typename AnyType>
+AnyType mult(AnyType First, int Second) {
+	return First * Second;
+}
+
+template <typename AnyType, typename AnyType2>
+AnyType sub(AnyType First, AnyType2 Second) {
+	return First - Second;
+}
+
+
 
 int main()
 {
 	cout << add(2, 3) << '\n';
 	cout << add(1.2, 3.4) << '\n';
+	
+	cout << mult(2, 3) << '\n';
+	cout << mult(1.2, 3) << '\n';
+	
+	cout << sub(3, 2) << '\n';
+	cout << sub(3.5, 2) << '\n';
+	cout << sub(4, 1.5) << '\n';
 
 }
