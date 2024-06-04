7ea2735e9d9b5c1925769ff31ccb3f1405f5f092
D63Store Owner <fleefymusic@gmail.com>
2024-06-04 14:22:55 +0300

    Create fixed_Task6.1.cpp

---
+++ fixed_Task6.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter a positive number: ";
+	int num{};
+	std::cin >> num;
+
+
+	if (num < 0) {
+		std::cout << "Negative number entered.  Making positive.\n";
+		num = -num;
+	} //fixed
+
+
+	std::cout << "You entered: " << num;
+
+	return 0;
+}
