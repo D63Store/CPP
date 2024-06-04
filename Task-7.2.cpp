d4eecd3641649cbcb3ef2c369fdd2d930f76fed6
D63Store Owner <fleefymusic@gmail.com>
2024-06-04 21:25:08 +0300

    Create Task-7.2.cpp

---
+++ Task-7.2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cassert>
+
+bool isPrime(int x)
+{
+    if (x <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= x / 2; ++i) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    assert(!isPrime(0));
+    assert(!isPrime(1));
+    assert(isPrime(2));
+    assert(isPrime(3));
+    assert(!isPrime(4));
+    assert(isPrime(5));
+    assert(isPrime(7));
+    assert(!isPrime(9));
+    assert(isPrime(11));
+    assert(isPrime(13));
+    assert(!isPrime(15));
+    assert(!isPrime(16));
+    assert(isPrime(17));
+    assert(isPrime(19));
+    assert(isPrime(97));
+    assert(!isPrime(99));
+    assert(!isPrime(99));
+    assert(isPrime(13417));
+
+    std::cout << "Success!";
+
+
+}
